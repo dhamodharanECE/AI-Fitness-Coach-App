3b9408837f0fa5a3bd8e4c9b78c8c0ec797ef85c
DHAMODHARAN S <dhamodharansece23@gmail.com>
2025-12-10 14:20:28 +0530

    Project

--- project/src/Api/data.js
+++ project/src/Api/data.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = 'https://ai-fitness-coach-app-project-48tt.onrender.com/api';
 export const generatePlan = async (userData) => {
   const response = await axios.post(`${API_BASE}/generate-plan`, userData);
   return response.data;
